65dd1e4e85f85cb0914cb96f3b794a1a22cc3434
abhic <abhijit.j.chaudhari@gmail.com>
2013-07-08 10:59:05 +0530

    maxmert installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maxmertapp::Application.config.secret_token = 'b2f14dd9ac409f4d30cc55d42e446793348893f2203054c868b14eef39e887edc9015d84ba6e971062612b173466f1e25bfa368d3ac55a1323e142f8bc2785cb'
